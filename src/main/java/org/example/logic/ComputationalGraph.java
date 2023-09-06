a4e36746c82c0321e2381af67cb870c3a7b2aa73
SeonWooJu <tjsdnwn21104@gmail.com>
2023-09-06 22:58:17 +0900

    기본 파일

---
+++ src/main/java/org/example/logic/ComputationalGraph.java
@@ -0,0 +1,19 @@
+package org.example.logic;
+
+import org.tensorflow.ConcreteFunction;
+import org.tensorflow.Signature;
+import org.tensorflow.Tensor;
+import org.tensorflow.internal.types.TFloat32Mapper;
+import org.tensorflow.op.Ops;
+import org.tensorflow.op.core.Placeholder;
+import org.tensorflow.op.math.Add;
+import org.tensorflow.types.TFloat32;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ComputationalGraph {
+    public ComputationalGraph() {
+
+    }
+}
