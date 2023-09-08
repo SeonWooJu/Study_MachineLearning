37655abe5b49e6e3fe899dedb99019f2f8e6fdd2
SeonWooJu <tjsdnwn21104@gmail.com>
2023-09-08 14:18:28 +0900

    주석 추가

--- study/TensorFlowHelloWorld.py
+++ study/TensorFlowHelloWorld.py
@@ -1,5 +1,10 @@
 import tensorflow as tf
 
+# Create a constant op
+# This op is added as a node to the default graph
 hello = tf.constant("Hello, TensorFlow!");
 
+# Versions above 2.0.0 do not use "tensorflow.Session()"
+
+# run the op end get result
 tf.print(hello)
